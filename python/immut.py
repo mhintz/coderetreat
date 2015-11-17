6503ef993979b19b114acf5918afd9160ca2aa98
Mark Hintz <markohintz@gmail.com>
2015-11-17 13:59:53 +0100

    Fix minor stuff so at least it executes (but doesn't do anything)

--- python/immut.py
+++ python/immut.py
@@ -4,10 +4,9 @@ import random
 def randState(): return random.randint(0, 1)
 
 def getBoard(side): return map(lambda x: map(lambda y: randState(), range(side)), range(side))
-
-  return [randState() for x in range(side) for y in range(side)]
+# return [randState() for x in range(side) for y in range(side)]
 
 class Tests():
   def test_board(self):
-
+    pass
 
